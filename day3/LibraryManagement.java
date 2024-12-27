ced30891e27adb9b614949b79245d3ee870f502e
komathisrinivasan <komathisrinivasan7@gmail.com>
2024-12-27 14:01:58 +0530

    Add files via upload

---
+++ day3/LibraryManagement.java
@@ -0,0 +1,12 @@
+package week1.day3;
+
+public class LibraryManagement {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Library AB = new Library();
+		
+		AB.addBook("Booktitle");
+		AB.issueBook();
+	}
+}
